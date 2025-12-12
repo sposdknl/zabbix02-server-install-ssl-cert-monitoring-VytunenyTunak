dc73c15769f2d8ca97705bee09f2c7b8da5b592d
Tomáš Ludvík <7tomas.ludvik@email.cz>
2025-12-12 09:33:57 +0100

    Add files via upload

---
+++ zabbix02-server-install-ssl-cert-monitoring-Ludvik/zabbix.conf.php
@@ -0,0 +1,10 @@
+<?php
+$DB['TYPE']     = 'MYSQL';
+$DB['SERVER']   = 'localhost';
+$DB['PORT']     = '0';
+$DB['DATABASE'] = 'zabbix';
+$DB['USER']     = 'zabbix';
+$DB['PASSWORD'] = 'zabbix';
+
+$ZBX_SERVER_NAME = 'Tomáš – Zabbix 7.0';
+?>
